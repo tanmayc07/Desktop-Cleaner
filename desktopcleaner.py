5f3f34168ef0ff60b667c5c6766e27c1f566efcf
tanmayc07 <tanmayc32@gmail.com>
2020-02-09 18:33:51 +0530

    Import watchdog modules

--- desktopcleaner.py
+++ desktopcleaner.py
@@ -1,1 +1,9 @@
-print("Hello World")
+import time
+from watchdog.observers import Observer
+from watchdog.events import PatternMatchingEventHandler
+
+if name == '__main__':
+    patterns = "*"
+    ignore_patterns = ""
+    ignore_directories = False
+    case_sensitive = False
